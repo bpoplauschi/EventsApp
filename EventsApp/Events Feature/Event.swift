3cb458ba9eb085793a420467052409c43d9e0a2c
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-07-14 15:53:20 +0300

    Created Event model

---
+++ EventsApp/Events Feature/Event.swift
@@ -0,0 +1,24 @@
+//
+//  Event.swift
+//  EventsApp
+//
+//  Created by Bogdan on 14.07.2022.
+//
+
+import Foundation
+
+public struct Event {
+    public let name: String
+    public let location: String
+    public let dateInterval: String
+    public let count: Int
+    public let imageURL: URL?
+    
+    public init(name: String, location: String, dateInterval: String, count: Int, imageURL: URL?) {
+        self.name = name
+        self.location = location
+        self.dateInterval = dateInterval
+        self.count = count
+        self.imageURL = imageURL
+    }
+}
