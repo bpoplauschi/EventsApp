d92565258122916b6a27fb5a9cdc270b3eea33a8
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-07-18 16:49:49 +0300

    `VividSeatsAPIEventsLoader` load delivers error on client error

--- EventsAppTests/VividSeatsAPIEventsLoaderTests.swift
+++ EventsAppTests/VividSeatsAPIEventsLoaderTests.swift
@@ -20,13 +20,19 @@ class VividSeatsAPIEventsLoader {
     
     typealias Result = EventsLoader.Result
     
+    enum Error: Swift.Error {
+        case connectivity
+    }
+    
     init(url: URL, httpClient: HTTPClient) {
         self.url = url
         self.httpClient = httpClient
     }
     
     func load(completion: @escaping (Result) -> Void) {
-        httpClient.post(to: url) { result in }
+        httpClient.post(to: url) { result in
+            completion(.failure(Error.connectivity))
+        }
     }
 }
 
@@ -56,6 +62,27 @@ class VividSeatsAPIEventsLoaderTests: XCTestCase {
         XCTAssertEqual(httpClient.requestedURLs, [url, url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, httpClient) = makeSUT()
+        let clientError = NSError(domain: "Test", code: 0)
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { result in
+            switch result {
+            case .success:
+                XCTFail("Expected failure, got success instead")
+                
+            case .failure(let error):
+                XCTAssertEqual(error as NSError, VividSeatsAPIEventsLoader.Error.connectivity as NSError)
+            }
+            exp.fulfill()
+        }
+        
+        httpClient.complete(with: clientError)
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     private func makeSUT(url: URL = URL(string: "http://a-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: VividSeatsAPIEventsLoader, httpClient: HTTPClientSpy) {
         let httpClient = HTTPClientSpy()
         let sut = VividSeatsAPIEventsLoader(url: url, httpClient: httpClient)
@@ -66,9 +93,15 @@ class VividSeatsAPIEventsLoaderTests: XCTestCase {
 }
 
 private class HTTPClientSpy: HTTPClient {
-    var requestedURLs: [URL] = []
+    private var messages: [(url: URL, completion: (HTTPClient.Result) -> Void)] = []
+    
+    var requestedURLs: [URL] { messages.map { $0.url } }
     
     func post(to url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-        requestedURLs.append(url)
+        messages.append((url, completion))
+    }
+    
+    func complete(with error: Error, at index: Int = 0) {
+        messages[index].completion(.failure(error))
     }
 }
