51abae15ab0dfb7f1cfcc6e52fd0071326e70dae
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-07-19 16:00:38 +0300

    Moved `HTTPClientSpy` type to helpers

---
+++ EventsAppTests/Events API/Helpers/HTTPClientSpy.swift
@@ -0,0 +1,28 @@
+//
+//  HTTPClientSpy.swift
+//  EventsAppTests
+//
+//  Created by Bogdan on 19.07.2022.
+//
+
+import Foundation
+import EventsApp
+
+final class HTTPClientSpy: HTTPClient {
+    private var messages: [(url: URL, completion: (HTTPClient.Result) -> Void)] = []
+    
+    var requestedURLs: [URL] { messages.map { $0.url } }
+    
+    func post(to url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+        messages.append((url, completion))
+    }
+    
+    func complete(with error: Error, at index: Int = 0) {
+        messages[index].completion(.failure(error))
+    }
+    
+    func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+        let response = HTTPURLResponse(url: messages[index].url, statusCode: code, httpVersion: nil, headerFields: nil)!
+        messages[index].completion(.success((data, response)))
+    }
+}
