116d602d3f742b540f2e35dd15ffacb1d9da1b90
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-07-19 17:12:09 +0300

    `RemoteImageDataLoader` load image data from URL delivers connectivity error on client error

--- EventsAppTests/Events API/RemoteImageDataLoaderTests.swift
+++ EventsAppTests/Events API/RemoteImageDataLoaderTests.swift
@@ -12,13 +12,19 @@ class RemoteImageDataLoader {
     private let url: URL
     private let httpClient: HTTPClient
     
+    enum Error: Swift.Error {
+        case connectivity
+    }
+    
     init(url: URL, httpClient: HTTPClient) {
         self.url = url
         self.httpClient = httpClient
     }
     
     func loadImageData(from url: URL, completion: @escaping (ImageDataLoader.Result) -> Void) -> ImageDataLoaderTask {
-        httpClient.get(from: url) { _ in }
+        httpClient.get(from: url) { _ in
+            completion(.failure(Error.connectivity))
+        }
         
         return ImageDataLoaderTaskSpy()
     }
@@ -52,6 +58,29 @@ class RemoteImageDataLoaderTests: XCTestCase {
         _ = sut.loadImageData(from: url) { _ in }
         
         XCTAssertEqual(httpClient.requestedURLs, [url, url])
+    }
+    
+    func test_loadImageDataFromURL_deliversConnectivityErrorOnClientError() {
+        let (sut, httpClient) = makeSUT()
+        let url = URL(string: "https://a-concrete-url.com")!
+        let clientError = anyNSError()
+        let exp = expectation(description: "Wait for load completion")
+        
+        _ = sut.loadImageData(from: url) { receivedResult in
+            switch receivedResult {
+            case let .failure(receivedError as RemoteImageDataLoader.Error):
+                XCTAssertEqual(receivedError, .connectivity)
+                
+            default:
+                XCTFail()
+            }
+            
+            exp.fulfill()
+        }
+        
+        httpClient.complete(with: clientError)
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     // MARK: - Helpers
