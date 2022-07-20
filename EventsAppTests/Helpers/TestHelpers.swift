4bdaaece292af1c71b5cd94fee3f85b8645cb7e3
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-07-20 16:28:19 +0300

    Updated helper `anyURL()` to include a .com extension

--- EventsAppTests/Helpers/TestHelpers.swift
+++ EventsAppTests/Helpers/TestHelpers.swift
@@ -9,4 +9,4 @@ import Foundation
 
 func anyData() -> Data { Data("any data".utf8) }
 func anyNSError() -> NSError { NSError(domain: "any error", code: 0) }
-func anyURL() -> URL { URL(string: "http://any-url")! }
+func anyURL() -> URL { URL(string: "http://any-url.com")! }
