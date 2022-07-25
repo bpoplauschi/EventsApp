202a40613bc0f3b2d3d5f8435a649ed57c00a737
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-07-25 15:05:55 +0300

    Created factory helper makeSUT

--- EventsAppTests/Events UI/EventsViewControllerTests.swift
+++ EventsAppTests/Events UI/EventsViewControllerTests.swift
@@ -26,15 +26,13 @@ class EventsViewController: UIViewController {
 
 class EventsViewControllerTests: XCTestCase {
     func test_init_doesNotLoadEvents() {
-        let loader = EventsLoaderSpy()
-        _ = EventsViewController(loader: loader)
+        let (_, loader) = makeSUT()
         
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
     func test_viewDidLoad_loadsEvents() {
-        let loader = EventsLoaderSpy()
-        let sut = EventsViewController(loader: loader)
+        let (sut, loader) = makeSUT()
         
         sut.loadViewIfNeeded()
         
@@ -43,7 +41,15 @@ class EventsViewControllerTests: XCTestCase {
     
     // MARK: - Helpers
     
-    class EventsLoaderSpy: EventsLoader {
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: EventsViewController, loader: EventsLoaderSpy) {
+        let loader = EventsLoaderSpy()
+        let sut = EventsViewController(loader: loader)
+        trackForMemoryLeaks(loader)
+        trackForMemoryLeaks(sut)
+        return (sut, loader)
+    }
+    
+    private class EventsLoaderSpy: EventsLoader {
         private(set) var loadCallCount: Int = 0
         
         func load(startDate: Date, endDate: Date, completion: @escaping (EventsLoader.Result) -> Void) {
