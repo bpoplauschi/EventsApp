8062088a98842ad2453fd74ac6d7e133aafd4ddb
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-07-25 15:38:11 +0300

    EventsViewController hides loading indicator on loader completion after pull-to-refresh

--- EventsAppTests/Events UI/EventsViewControllerTests.swift
+++ EventsAppTests/Events UI/EventsViewControllerTests.swift
@@ -84,6 +84,15 @@ class EventsViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
     }
     
+    func test_pullToRefresh_hidesLoadingIndicatorOnLoaderCompletion() {
+        let (sut, loader) = makeSUT()
+        
+        sut.refreshControl?.simulatePullToRefresh()
+        loader.complete()
+        
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: EventsViewController, loader: EventsLoaderSpy) {
