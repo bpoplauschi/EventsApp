991a270d11a2650cc7b034ba0e0a3acaff6527fd
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-07-25 15:29:04 +0300

    EventsViewController loads events on pull-to-refresh

--- EventsAppTests/Events UI/EventsViewControllerTests.swift
+++ EventsAppTests/Events UI/EventsViewControllerTests.swift
@@ -9,7 +9,7 @@ import EventsApp
 import UIKit
 import XCTest
 
-class EventsViewController: UIViewController {
+class EventsViewController: UITableViewController {
     private var loader: EventsLoader?
     
     convenience init(loader: EventsLoader) {
@@ -20,6 +20,12 @@ class EventsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        refresh()
+    }
+    
+    @objc private func refresh() {
         loader?.load(startDate: Date(), endDate: Date()) { _ in }
     }
 }
@@ -39,6 +45,17 @@ class EventsViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 1)
     }
     
+    func test_pullToRefresh_loadsEvents() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(loader.loadCallCount, 2)
+        
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(loader.loadCallCount, 3)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: EventsViewController, loader: EventsLoaderSpy) {
@@ -54,6 +71,16 @@ class EventsViewControllerTests: XCTestCase {
         
         func load(startDate: Date, endDate: Date, completion: @escaping (EventsLoader.Result) -> Void) {
             loadCallCount += 1
+        }
+    }
+}
+
+private extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
         }
     }
 }
