be842b7d237e6b66a00817875d064c7cdee1664c
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-08-02 11:58:29 +0300

    UI updates including gradient view

---
+++ EventsApp/Events UI/Views/GradientView.swift
@@ -0,0 +1,33 @@
+//
+//  GradientView.swift
+//  EventsApp
+//
+//  Created by Bogdan on 02.08.2022.
+//
+
+import UIKit
+
+@IBDesignable final class GradientView: UIView {
+    @IBInspectable var firstColor: UIColor = .white {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var secondColor: UIColor = .black {
+        didSet {
+            updateView()
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+       get {
+          return CAGradientLayer.self
+       }
+    }
+    
+    private func updateView() {
+        guard let gradientLayer = self.layer as? CAGradientLayer else { return }
+        gradientLayer.colors = [firstColor, secondColor].map { $0.cgColor }
+    }
+}
